0b189fca087e219b5ea7f6757cc75a5536b9059f
DrWatson <no@mail>
2021-11-09 00:51:50 +0100

    now it works

--- src/anton-pde.jl
+++ src/anton-pde.jl
@@ -13,9 +13,11 @@ Dt = Differential(t)
 
 heaviside(x) = 0.5 * (sign(x) + 1)
 v(α) = 2 * cot(2π * α)
-δ(x, ϵ = 1e-1) = exp(-x^2 / (2ϵ^2)) / sqrt(2π * ϵ^2)
+δ(x, ϵ = 1e-2) = exp(-x^2 / (2ϵ^2)) / sqrt(2π * ϵ^2)
 
-eq(α) = Dt(w(t, x, y)) ~ Dxx(w(t, x, y)) + Dyy(w(t, x, y)) + heaviside(w(t,x,y) - α)* δ(y)  #+  v(α) * Dx(w(t, x, y)) - w(t, x, y) 
+eq(α) = Dt(w(t, x, y)) ~ Dxx(w(t, x, y)) + Dyy(w(t, x, y)) + heaviside(w(t,x,y) - α)* δ(y) - w(t, x, y) - v(α) * Dx(w(t, x, y)) 
+
+sigmoid(x) = 1/(1+exp(-x))
 
 function solver(α, p)
        # Initial and boundary conditions
@@ -24,7 +26,7 @@ function solver(α, p)
               w(t, -L, y) ~ 0.,# for all t > 0
               w(t, x, L) ~ 0.,# for all t > 0
               w(t, x, -L) ~ 0.,# for all t > 0
-              w(0, x, y) ~ exp(-x^2-y^2)
+              w(0, x, y) ~ sigmoid(-x)
               ]
 
        # Space and time domains
@@ -33,7 +35,6 @@ function solver(α, p)
               x ∈ Interval(-L, L),
               y ∈ Interval(-L, L)]
 
-
        # Method of lines discretization
        dx = dy = p[:dx]
        discretization = MOLFiniteDifference([x => dx, y => dy], t)
@@ -41,18 +42,18 @@ function solver(α, p)
        @named pde_system = PDESystem(eq(α), bcs, domains, [t,x,y], [w(t, x, y)])
        prob = discretize(pde_system, discretization)
 
-       # solver ODE
-       sol = solve(prob)
+       # Solve ODE
+       sol = solve(prob, ABDF2())
        
        xs, ys = [infimum(d.domain):dx:supremum(d.domain) for d in domains[2:end]]
        return (x = xs, y = ys, w = map(s -> transpose(reshape(s, (length(xs)-2, length(ys)-2))), sol.u))
 end
 
-p = Dict(
-       :L => 20.,
-       :T => 100.,
-       :dx => 1.
-       )
-sol = solver(.4, p)
-contourf(sol.w[100], aspect_ratio = 1)
+sol = solver(.4, Dict(
+                     :L => 10.,
+                     :T => 10.,
+                     :dx => 1.
+                     )
+              )
+contourf(sol.w[end], aspect_ratio = 1, ylims = (7,13))
 
